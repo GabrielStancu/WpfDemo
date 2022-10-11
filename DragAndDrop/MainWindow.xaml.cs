3f5914d0c4b546881bdc006767c98293b3e18362
GabrielStancu <gabriel.stancu07@yahoo.com>
2022-10-11 21:55:19 +0300

    Added drag and drop of objects between different canvas

--- DragAndDrop/MainWindow.xaml.cs
+++ DragAndDrop/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DragAndDrop;
 /// <summary>
@@ -22,21 +9,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void RedRectangle_MouseMove(object sender, MouseEventArgs e)
-    {
-        if (e.LeftButton == MouseButtonState.Pressed)
-        {
-            DragDrop.DoDragDrop(RedRectangle, RedRectangle, DragDropEffects.Move);
-        }
-    }
-
-    private void Canvas_DragOver(object sender, DragEventArgs e)
-    {
-        Point dropPosition = e.GetPosition(DragCanvas);
-
-        Canvas.SetLeft(RedRectangle, dropPosition.X);
-        Canvas.SetTop(RedRectangle, dropPosition.Y);
     }
 }
---
+++ DragAndDrop/Views/Canvas.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DragAndDrop.Views;
+/// <summary>
+/// Interaction logic for CanvasView.xaml
+/// </summary>
+public partial class Canvas : UserControl
+{
+    public static readonly DependencyProperty IsChildHitTestVisibleProperty = 
+        DependencyProperty.Register(nameof(IsChildHitTestVisible), typeof(bool), typeof(Canvas), new PropertyMetadata(true));
+
+    public bool IsChildHitTestVisible
+    {
+        get => (bool)GetValue(IsChildHitTestVisibleProperty);
+        set => SetValue(IsChildHitTestVisibleProperty, value);
+    }
+
+    public static readonly DependencyProperty ColorProperty =
+        DependencyProperty.Register(nameof(Color), typeof(Brush), typeof(Canvas), new PropertyMetadata(Brushes.Black));
+
+    public Brush Color
+    {
+        get => (Brush)GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+
+    public Canvas()
+    {
+        InitializeComponent();
+    }
+
+    private void Rectangle_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (e.LeftButton != MouseButtonState.Pressed) 
+            return;
+
+        IsChildHitTestVisible = false;
+        DragDrop.DoDragDrop(Rectangle, new DataObject(DataFormats.Serializable, Rectangle), DragDropEffects.Move);
+        IsChildHitTestVisible = true;
+    }
+
+    private void Canvas_DragOver(object sender, DragEventArgs e)
+    {
+        var data = e.Data.GetData(DataFormats.Serializable);
+
+        if (data is not UIElement element)
+            return;
+
+        var dropPosition = e.GetPosition(DragCanvas);
+
+        System.Windows.Controls.Canvas.SetLeft(element, dropPosition.X);
+        System.Windows.Controls.Canvas.SetTop(element, dropPosition.Y);
+
+        if (DragCanvas.Children.Contains(element))
+            return;
+
+        DragCanvas.Children.Add(element);
+    }
+
+    private void Canvas_DragLeave(object sender, DragEventArgs e)
+    {
+        if (!e.OriginalSource.Equals(DragCanvas))
+            return;
+
+        object? data = e.Data.GetData(DataFormats.Serializable);
+
+        if (data is UIElement element)
+        {
+            DragCanvas.Children.Remove(element);
+        }
+    }
+}
